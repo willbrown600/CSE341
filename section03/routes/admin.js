c42aa4ec9d0db28d06372e5ca60bc27735517ffb
Will Brown <bro18085@byui.edu>
2022-01-18 19:04:25 +1000

    update section 3

--- section03/routes/admin.js
+++ section03/routes/admin.js
@@ -2,16 +2,23 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-// /admin/add-product => get
-router.get('/add-product', productsController.getAddProduct);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
-// /admin/add-product => post
-router.post('/add-product', productsController.postAddProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
